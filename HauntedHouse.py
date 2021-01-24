dd99b3882c96434c1f74ce5bfebe0018c72ee5c9
Jwoolter <jwoolter@gmail.com>
2021-01-24 09:50:04 +0000

    added use and drop

--- HauntedHouse.py
+++ HauntedHouse.py
@@ -5,8 +5,7 @@ WEST = "WEST"
 UP = "UP"
 DOWN = "DOWN"
 NORTHWEST = "NORTH WEST"
-key = True
-torch = True
+
 
 ROOMS = {0: ["PORCH", "you stand", True],
          1: ["ENTRANCE HALL", "entrance", True],
@@ -18,25 +17,23 @@ ROOMS = {0: ["PORCH", "you stand", True],
          7: ["CELLAR", "spooky", False],
          8: ["STAIR 1F", "climb", True],
          9: ["LANDING 1F", "you stand", True],
-         10:["MASTER BEDROOM", "sleep", True],
-         11:["DRESSING ROOM", "dress", True],
-         12:["BATHROOM", "wash", False],
-         13:["STUDY", "work", True],
-         14:["STAIR 2F", "climb", True],
-         15:["LANDING 2F", "you stand", True],
-         16:["CHILD BEDROOM", "sleep", True],
-         17:["PLAYROOM", "play", True],
-         18:["CHILD BATHROOM", "wash", True],
-         19:["STAIR 3F", "climb", True],
-         20:["ATTIC", "dust", False],
-         21:["TRAPDOOR", "trapped", True],
-         22:["STAIR BF", "climb", True],
-         23:["LANDING BF", "you stand", False],
-         24:["DUNGEON", "scary", False],
-         25:["WINE CELLAR", "drink", True],
-         26:["CELL", "lock", True],
-
-
+         10: ["MASTER BEDROOM", "sleep", True],
+         11: ["DRESSING ROOM", "dress", True],
+         12: ["BATHROOM", "wash", False],
+         13: ["STUDY", "work", True],
+         14: ["STAIR 2F", "climb", True],
+         15: ["LANDING 2F", "you stand", True],
+         16: ["CHILD BEDROOM", "sleep", True],
+         17: ["PLAYROOM", "play", True],
+         18: ["CHILD BATHROOM", "wash", True],
+         19: ["STAIR 3F", "climb", True],
+         20: ["ATTIC", "dust", False],
+         21: ["TRAPDOOR", "trapped", True],
+         22: ["STAIR BF", "climb", False],
+         23: ["LANDING BF", "you stand", False],
+         24: ["DUNGEON", "scary", False],
+         25: ["WINE CELLAR", "drink", False],
+         26: ["CELL", "lock", False],
 
          999: ["you win", "you win", True]}
 
@@ -49,117 +46,218 @@ CONNECTIONS = {0: {NORTH: [1, False]},
                6: {SOUTH: [1, False], UP: [8, False], DOWN: [22, False]},
                8: {SOUTH: [9, False], UP: [14, False], DOWN: [6, False]},
                9: {NORTH: [8, False], EAST: [10, False], WEST: [13, True]},
-               10:{NORTH: [11, False], WEST: [9, False]},
-               11:{SOUTH: [10,False], WEST: [12, True]},
-               12:{EAST: [11, True]},
-               13:{EAST: [9, True]},
-               14:{SOUTH: [15, False], UP: [19, False], DOWN: [8, False]},
-               15:{NORTH: [14, False], EAST: [16, True], WEST: [17, False]},
-               16:{NORTHWEST: [18, False], WEST: [15, True]},
-               17:{EAST: [15, False]},
-               18:{EAST: [16, False]},
-               19:{SOUTH: [20, False], DOWN: [14, False]},
-               20:{NORTH: [19, False], WEST: [21, True]},
-               21:{DOWN: [26, False]},
-               22:{SOUTH: [23, False], UP: [6, False]},
-               23:{NORTH: [22, False], EAST: [25, False], WEST: [24, True]},
-               24:{NORTH: [26, True], EAST: [23, True]},
-               25:{EAST: [23, False]},
-               26:{SOUTH: [24, True]}
-
-
+               10: {NORTH: [11, False], WEST: [9, False]},
+               11: {SOUTH: [10, False], WEST: [12, True]},
+               12: {EAST: [11, True]},
+               13: {EAST: [9, True]},
+               14: {SOUTH: [15, False], UP: [19, False], DOWN: [8, False]},
+               15: {NORTH: [14, False], EAST: [16, True], WEST: [17, False]},
+               16: {NORTHWEST: [18, False], WEST: [15, True]},
+               17: {EAST: [15, False]},
+               18: {EAST: [16, False]},
+               19: {SOUTH: [20, False], DOWN: [14, False]},
+               20: {NORTH: [19, False], WEST: [21, True]},
+               21: {DOWN: [26, False]},
+               22: {SOUTH: [23, False], UP: [6, False]},
+               23: {NORTH: [22, False], EAST: [25, False], WEST: [24, True]},
+               24: {NORTH: [26, True], EAST: [23, True]},
+               25: {EAST: [23, False]},
+               26: {SOUTH: [24, True]}
                }
-OBJECTS = {0:  ["Torch", 10, 2],
-           1:  ["Key", 3, 1]}
 
-OBJECT_LOCATIONS = {0: 17,
-                    1: 11}
+# name, size, weight
+OBJECTS = {0: ["Lit Torch", 10, 2],
+           1: ["Key", 3, 1],
+           2: ["Unlit Torch", 10, 2],
+           3: ["Matches", 1, 1],
+           4: ["Bronze Coin", 1, 1],
+           5: ["Silver Coin", 1, 1],
+           6: ["Gold Coin", 1, 1],
+           7: ["Chalice", 5, 3],
+           8: ["Matches", 1, 1],
+           9: ["Matches", 1, 1],
+           10: ["Matches", 1, 1],
+
+           }
 
 INVENTORY_LOCATION = 7
 
+VOID_LOCATION = 27
+
+OBJECT_LOCATIONS = {0: VOID_LOCATION,
+                    1: 11,
+                    2: 17,
+                    3: 4,
+                    4: 2,
+                    5: 8,
+                    6: 18,
+                    7: INVENTORY_LOCATION,
+                    8: 14,
+                    9: 20,
+                    10: 3
+                    }
+
+INVENTORY_LOCATION = 7
+
+VOID_LOCATION = 27
+
+
 def objectsAtLocation(roomID):
     found = []
-    for k,v in OBJECT_LOCATIONS.items():
+    for k, v in OBJECT_LOCATIONS.items():
         if v == roomID:
             found.append(k)
     return (found)
+
 
 def prtRoom(roomID):
     name, description, isLit = ROOMS[roomID]
     directions = CONNECTIONS.get(roomID)
 
+def use(objectID, currentRoomID):
+    inventory = objectsAtLocation(INVENTORY_LOCATION)
+    success = False
+    name, size, weight = OBJECTS[objectID]
+    print(f"you try to use {name}")
 
-
-
-
-
-
-
-
-def inventory():
-    objects = objectsAtLocation(INVENTORY_LOCATION)
-    if len(objects) >= 1:
-        print("This is your stuff")
-        for i, k in enumerate(objects):
-            name, size, weight = OBJECTS[k]
-            print(f"{i + 1}) {name}: {size} unit(s) big, {weight} unit(s) heavy")
-            x=input()
-    else:
-        print("you have no stuff!")
+    if name == "UnLit Torch":
+        print("you wave an un lit torch")
         x=input()
+        print("it does nothing")
+        success = True
+    elif name == "Matches":
+        print("you light a match")
+        OBJECT_LOCATIONS[objectID] = VOID_LOCATION
+        if OBJECT_LOCATIONS[2] == INVENTORY_LOCATION:
+            OBJECT_LOCATIONS[2] = VOID_LOCATION
+            OBJECT_LOCATIONS[0] = INVENTORY_LOCATION
+            print("the match lit your torch!")
+            success = True
+
+    if success is False:
+        x=input()
+        print("nothing happend")
+
+
+
+def inventory(currentRoomID):
+    objects = objectsAtLocation(INVENTORY_LOCATION)
+    valid = False
+    while valid is False:
+        print("What would you like to do with your items?\n1) USE\n2) DROP\n3) VIEW\n4) EXIT")
+        ans = int(input(""))
+        if ans == 1:
+            if len(objects) >= 1:
+                valid = False
+                while valid is False:
+                    print("What would you like to use?")
+                    for i, k in enumerate(objects):
+                        name, size, weight = OBJECTS[k]
+                        print(f"{i + 1}) {name}: {size} unit(s) big, {weight} unit(s) heavy")
+                    ans = int(input(""))
+                    if ans > 0 and ans <= len(objects):
+                        selectedObject = objects[ans - 1]
+                        use(selectedObject, currentRoomID)
+
+                        x = input()
+                        valid = True
+
+            else:
+                print("you have no stuff!")
+        elif ans == 2:
+            if len(objects) >= 1:
+                valid = False
+                while valid is False:
+                    print("What would you like to drop?")
+                    for i, k in enumerate(objects):
+                        name, size, weight = OBJECTS[k]
+                        print(f"{i + 1}) {name}: {size} unit(s) big, {weight} unit(s) heavy")
+                    ans = int(input(""))
+                    if ans > 0 and ans <= len(objects):
+                        selectedObject = objects[ans - 1]
+                        name, size, weight = OBJECTS[selectedObject]
+                        OBJECT_LOCATIONS[selectedObject] = currentRoomID
+                        print(f"you dropped {name}")
+                        x = input()
+                        valid = True
+                    else:
+                        print("that is not the answer you are looking for")
+                        x=input()
+
+
+
+            else:
+                print("you have no stuff!")
+        elif ans == 3:
+            if len(objects) >= 1:
+                print("This is your stuff")
+                for i, k in enumerate(objects):
+                    name, size, weight = OBJECTS[k]
+                    print(f"{i + 1}) {name}: {size} unit(s) big, {weight} unit(s) heavy")
+                x = input()
+
+            else:
+                print("you have no stuff!")
+                x = input()
+        else:
+            valid = True
 
 def objects(roomID):
     objects = objectsAtLocation(roomID)
     if len(objects) >= 1:
         print("what object would you like to pick up?")
         for i, k in enumerate(objects):
-            name,size,weight = OBJECTS[k]
-            print(f"{i+1}) {name}")
+            name, size, weight = OBJECTS[k]
+            print(f"{i + 1}) {name}")
         ans = int(input(""))
-        selectedObject = objects[ans-1]
-        name,size,weight = OBJECTS[selectedObject]
+        selectedObject = objects[ans - 1]
+        name, size, weight = OBJECTS[selectedObject]
         print(f"you pick up {name}")
         OBJECT_LOCATIONS[selectedObject] = INVENTORY_LOCATION
-        x=input()
+        x = input()
     else:
         print("there are no object you can see at the moment")
-        x=input()
+        x = input()
+
 
 def move(roomID):
     name, description, isLit = ROOMS[roomID]
+    hasKey = OBJECT_LOCATIONS[1] == INVENTORY_LOCATION
+    hasTorch = OBJECT_LOCATIONS[0] == INVENTORY_LOCATION
     directions = CONNECTIONS.get(roomID)
-    if directions is not None and isLit is True or torch is True:
+    if directions is not None and isLit is True or hasTorch is True:
         choices = list(directions.keys())
         print("Where would you like to go?")
         for i, k in enumerate(choices):
             destinationID, isLocked = directions[k]
-            name,description, isLit = ROOMS[destinationID]
-            if isLocked is False or key is True:
-                print(f"{i+1}) {k}: {name}")
+            name, description, isLit = ROOMS[destinationID]
+            if isLocked is False or hasKey is True:
+                print(f"{i + 1}) {k}: {name}")
             elif isLocked is True:
-                print(f"{i+1}) {k}: LOCKED")
+                print(f"{i + 1}) {k}: LOCKED")
 
         valid = False
         while valid is False:
             ans = int(input(""))
-            if ans <=len(choices) and ans >= 1:
+            if ans <= len(choices) and ans >= 1:
                 valid = True
-            elif torch is False and isLit is True:
+            elif hasTorch is False and isLit is True:
                 print("that room is dark and scary")
             else:
                 print("that is not the answer you are looking for")
-        chosenDirection = choices[ans-1]
+        chosenDirection = choices[ans - 1]
         destination, isLocked = directions[chosenDirection]
-        if isLocked is False or key is True:
+        if isLocked is False or hasKey is True:
             return destination
         else:
             print("the door is locked")
-            x=input()
-            return(roomID)
+            x = input()
+            return (roomID)
 
     else:
         x = input()
         return (roomID)
+
 
 def main():
     currentRoomID = 0
@@ -173,7 +271,8 @@ def main():
 
                 nextRoomID = move(currentRoomID)
                 name, description, isLit = ROOMS[nextRoomID]
-                if isLit is True or torch is True:
+                hasTorch = OBJECT_LOCATIONS[0] == INVENTORY_LOCATION
+                if isLit is True or hasTorch is True:
                     currentRoomID = nextRoomID
                 else:
                     print("this room is dark and scary")
@@ -183,10 +282,7 @@ def main():
                 objects(currentRoomID)
             elif ans == 3:
                 valid = True
-                inventory()
-
-
-
+                inventory(currentRoomID)
 
 
 main()
